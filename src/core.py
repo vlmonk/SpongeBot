aefa34d4c524b6d020c7633a3ffb097ac4e154b5
Vadim Iskuchekov <vadim@Nucleon.local>
2015-07-12 23:18:31 +1000

    Хот фикс.

--- src/core.py
+++ src/core.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import unicode_literals, print_function
 import time
 
 from conf import USERS, URL, TOKEN, HELP_TEXT
