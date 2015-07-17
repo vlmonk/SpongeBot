af339a7670489e1a47d68ed2c154ea4037d3d3eb
Deus Egregors <vadim.deus@gmail.com>
2015-07-17 19:27:21 +1000

    Интерактивный режим для написания мессаг от имени бота

--- tools/bot.py
+++ tools/bot.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function
+
 import click
 import requests
 
@@ -6,10 +9,28 @@ import requests
 @click.option('-T', '--token', help='Your Telegram API TOKEN', prompt='Your API token')
 @click.option('-c', '--chat_id', default=-10725690, help='ID of target Chat', prompt='ID of target Chat')
 @click.option('-t', '--text', help='text to send', prompt='Text to send')
-def send(token, chat_id, text):
+@click.option('-i', '--interactive', help='', is_flag=True, default=False)
+def send(token, chat_id, text, interactive):
     data = {'chat_id': chat_id, 'text': text}
     request = requests.post('https://api.telegram.org/bot' + token + '/sendMessage', data=data)
-    print('bot: done') if request.status_code == 200 else print('bot: fail')
+    print('bot: OK') if request.status_code == 200 else print('bot: FAILED')
+
+    if interactive:
+        print('To exit press CTRL + C')
+        status = '[last msg status..]'
+        while True:
+            try:
+                t = input(status + ' TEXT: ')
+                data = {'chat_id': chat_id, 'text': t}
+                request = requests.post('https://api.telegram.org/bot' + token + '/sendMessage', data=data)
+            except Exception as err:
+                status = '[FAILED: ' + str(err) + ']'
+                print(status)
+
+            if request.status_code == 200:
+                status = '[OK]'
+            else:
+                status = '[FAILED]'
 
 
 if __name__ == '__main__':
