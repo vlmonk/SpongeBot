d02a97ebff5bcf76c39427510459b4386295806b
Vadim Iskuchekov <vadim.deus@gmail.com>
2015-08-04 16:05:28 +1000

    Курсы юаня и цена на нефть

--- src/cmd/show_me.py
+++ src/cmd/show_me.py
@@ -49,41 +49,55 @@ def show_me_currency():
     """
     g_usd = Grab()
     g_eur = Grab()
+    g_cny = Grab()
+    g_fuel = Grab()
 
-    # USD
     USD_URL = 'https://news.yandex.ru/quotes/2002.html'
+    EUR_URL = 'https://news.yandex.ru/quotes/2000.html'
+    CNY_URL = 'https://news.yandex.ru/quotes/10018.html'
+    FUEL_URL = 'https://news.yandex.ru/quotes/1006.html'
 
     try:
         g_usd.go(USD_URL)
+        g_eur.go(EUR_URL)
+        g_cny.go(CNY_URL)
+        g_fuel.go(FUEL_URL)
     except:
         return 'FAIL'
 
     resp = g_usd.doc.select('//tr[@class="quote__head"]')
-
     USD = {
         'date': resp.select('//td[@class="quote__date"]').text(),
         'value': resp.select('//td[@class="quote__value"]').text(),
         'change': resp.select('//td[@class="quote__change"]').text(),
     }
 
-    # EURO
-    EUR_URL = 'https://news.yandex.ru/quotes/2000.html'
-    try:
-        g_eur.go(EUR_URL)
-    except:
-        return 'FAIL'
-
     resp = g_eur.doc.select('//tr[@class="quote__head"]')
-
     EURO = {
         'date': resp.select('//td[@class="quote__date"]').text(),
         'value': resp.select('//td[@class="quote__value"]').text(),
         'change': resp.select('//td[@class="quote__change"]').text(),
     }
 
-    return '[{usd_date}]\n' \
-           'USD: {usd_val} ({usd_change})\n' \
-           'EUR: {eur_val} ({eur_change})\n'.format(usd_date=USD['date'], usd_val=USD['value'],
-                                                    usd_change=USD['change'],
-                                                    eur_date=EURO['date'], eur_val=EURO['value'],
-                                                    eur_change=EURO['change'])
+    resp = g_cny.doc.select('//tr[@class="quote__head"]')
+    CNY = {
+        'date': resp.select('//td[@class="quote__date"]').text(),
+        'value': resp.select('//td[@class="quote__value"]').text(),
+        'change': resp.select('//td[@class="quote__change"]').text(),
+    }
+
+    resp = g_fuel.doc.select('//tr[@class="quote__head"]')
+    FUEL = {
+        'date': resp.select('//td[@class="quote__date"]').text(),
+        'value': resp.select('//td[@class="quote__value"]').text(),
+        'change': resp.select('//td[@class="quote__change"]').text(),
+    }
+
+    return 'USD: {usd_val} ({usd_change})\n' \
+           'EUR: {eur_val} ({eur_change})\n' \
+           'CNY: {cny_val} ({cny_change})\n' \
+           '=================\n' \
+           'Brent: {fuel_val} ({fuel_change})'.format(usd_val=USD['value'], usd_change=USD['change'],
+                                                      eur_val=EURO['value'], eur_change=EURO['change'],
+                                                      cny_val=CNY['value'], cny_change=CNY['change'],
+                                                      fuel_val=FUEL['value'], fuel_change=FUEL['change'])
